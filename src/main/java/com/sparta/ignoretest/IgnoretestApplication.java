f0c28a883474c47072d35ae6a43605db456d77ae
CGYOON <ckdrmsdl9999@naver.com>
2025-02-14 13:44:04 +0900

    ignore not how

---
+++ src/main/java/com/sparta/ignoretest/IgnoretestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ignoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnoretestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgnoretestApplication.class, args);
+    }
+
+}
